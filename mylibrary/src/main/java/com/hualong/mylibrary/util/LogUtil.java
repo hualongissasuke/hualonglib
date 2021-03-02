07115dd5ee3e69f32a532915276e8ea0d20bc951
hualongissasuke <1024593500@qq.com>
2021-03-02 10:54:31 +0800

    第一次提交

---
+++ mylibrary/src/main/java/com/hualong/mylibrary/util/LogUtil.java
@@ -0,0 +1,29 @@
+package com.hualong.mylibrary.util;
+
+import android.util.Log;
+
+public class LogUtil {
+
+    public static final String LOG_NAME ="hualong";
+
+    public static void d(String ...msg){
+        if(msg == null || msg.length == 0){
+            Log.d(LOG_NAME,"msg is Empty");
+        }else{
+            StringBuffer sb = new StringBuffer();
+            for(String str : msg){
+                sb.append(str).append(",");
+            }
+            Log.d("hualong",sb.substring(0,sb.length()-1));
+        }
+    }
+
+    public static void d(int ...num){
+        StringBuffer sb = new StringBuffer();
+        for(int n : num){
+            sb.append(n).append(",");
+        }
+        Log.d("hualong",sb.substring(0,sb.length()-1));
+    }
+
+}
