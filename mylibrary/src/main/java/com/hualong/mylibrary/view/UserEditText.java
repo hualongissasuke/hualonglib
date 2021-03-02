a2f91ed95d5542fd75e101150306df6e8f845ab1
hualongissasuke <1024593500@qq.com>
2021-03-02 14:48:03 +0800

    修改UserEditText

--- mylibrary/src/main/java/com/hualong/mylibrary/view/UserEditText.java
+++ mylibrary/src/main/java/com/hualong/mylibrary/view/UserEditText.java
@@ -8,22 +8,27 @@ import android.util.AttributeSet;
 
 import androidx.appcompat.widget.AppCompatEditText;
 
-import org.w3c.dom.Text;
 
 public class UserEditText extends AppCompatEditText {
     public UserEditText(Context context) {
-        this(context,null);
+        super(context);
+        init();
     }
 
     public UserEditText(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        super(context, attrs);
+        init();
     }
 
     public UserEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
+        init();
+    }
+
+    private void init() {
         int inputType = getInputType();
-        if(inputType == InputType.TYPE_CLASS_TEXT || inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD){
+        if (inputType == InputType.TYPE_CLASS_TEXT || inputType == InputType.TYPE_TEXT_VARIATION_PASSWORD) {
             this.addTextChangedListener(new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -37,12 +42,12 @@ public class UserEditText extends AppCompatEditText {
 
                 @Override
                 public void afterTextChanged(Editable s) {
-                    if(s.length() > 0){
-                        for(int i = 0; i < s.length(); i++){
+                    if (s.length() > 0) {
+                        for (int i = 0; i < s.length(); i++) {
                             char c = s.charAt(i);
                             if (c >= 0x4e00 && c <= 0X9fff) { // 根据字节码判断(中文字节码范围)
                                 // 如果是中文，则清除输入的字符，否则保留
-                                s.delete(i,i+1);
+                                s.delete(i, i + 1);
                             }
                         }
                     }
